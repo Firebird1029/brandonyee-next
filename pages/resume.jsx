8e9569fe8198730eb6f7ab62e3123a415a7beaf5
Brandon Yee <6111102+Firebird1029@users.noreply.github.com>
2022-12-29 21:35:00 -1000

    add basic Resume page

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -1,12 +1,113 @@
+// use https://affinda.com/resume-parser/ to parse resume
+import ReactMarkdown from "react-markdown";
+// import remarkGfm from "remark-gfm";
 import Header from "../components/Header";
-// import styles from "../styles/Resume.module.css";
+import styles from "../styles/Resume.module.css";
+import resumeData from "../resume.json";
+import { projects as projectData } from "../projects.js";
+
+// Re-format Affinda-parsed dates (MM-DD-YY) to pretty date
+const MONTH_NAME_MAP = {
+	"01": "January",
+	"02": "February",
+	"03": "March",
+	"04": "April",
+	"05": "May",
+	"06": "June",
+	"07": "July",
+	"08": "August",
+	"09": "September",
+	10: "October",
+	11: "November",
+	12: "December",
+};
+
+function parseDate(date) {
+	const [year, month] = date.split("-");
+	return `${MONTH_NAME_MAP[month].substring(0, 3)} ${year}`;
+}
 
 export default function Resume() {
 	return (
-		<>
-			<main className="app">
-				<Header />
-			</main>
-		</>
+		<main className="app">
+			<Header />
+			<p>&nbsp;</p>
+			<div className="sticky-container">
+				<div className={styles.sectionHeader}>
+					<h2 className="blue-text" style={{ margin: 0 }}>
+						WORK EXPERIENCE
+					</h2>
+				</div>
+				{resumeData.workExperience.map(
+					({ id, jobTitle, jobDescription, organization, dates, location }) => (
+						<div key={id}>
+							<div className={styles.resumeSectionFlex}>
+								{/* Job Title + Company */}
+								<div className={styles.jobTitleCompany}>
+									<strong>{jobTitle}</strong>
+
+									<span className="hidden-touch">
+										&nbsp;&nbsp;/&nbsp;&nbsp;
+									</span>
+									<br className="hidden-desktop" />
+
+									<em>{organization}</em>
+								</div>
+
+								{/* Job Date + Location */}
+								<div>
+									<strong>{parseDate(dates.startDate)}</strong>
+									<span>&nbsp;&#8211;&nbsp;</span>
+									<strong>{parseDate(dates.endDate)}</strong>
+
+									<span className="hidden-touch">
+										&nbsp;&nbsp;/&nbsp;&nbsp;
+									</span>
+									<br className="hidden-desktop" />
+
+									<em>{location ? location.rawInput : "Remote"}</em>
+								</div>
+							</div>
+
+							{/* Job Description */}
+							<div style={{ marginTop: "-0.5rem" }}>
+								<div className="markdown" style={{ marginBottom: "2rem" }}>
+									<ReactMarkdown>
+										{jobDescription.replaceAll("\n-", "\n- ")}
+									</ReactMarkdown>
+								</div>
+							</div>
+						</div>
+					)
+				)}
+			</div>
+			<p>&nbsp;</p>
+			<div className="sticky-container">
+				<div className={styles.sectionHeader}>
+					<h2 className="blue-text" style={{ margin: 0 }}>
+						PROJECTS
+					</h2>
+				</div>
+				{projectData.map(({ title, description, year, id }) => (
+					<div key={id}>
+						<div className={styles.resumeSectionFlex}>
+							<div className={styles.jobTitleCompany}>
+								<strong>{title}</strong>
+
+								<span className="hidden-touch">&nbsp;&nbsp;/&nbsp;&nbsp;</span>
+								<br className="hidden-desktop" />
+
+								<em>{year}</em>
+							</div>
+						</div>
+						<div style={{ marginTop: "-0.5rem" }}>
+							<div className="markdown" style={{ marginBottom: "2rem" }}>
+								<ReactMarkdown>{description}</ReactMarkdown>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+		</main>
 	);
 }
---
+++ projects.js
@@ -0,0 +1,112 @@
+export const projects = [
+	// Distance Thermometer
+	{
+		title: "&#x1F6E1; Distance Thermometer",
+		// "linkHref": "https://distancethermo.com/",
+		// "linkText": "distancethermo.com",
+		description:
+			"A physical noncontact infrared thermometer with wireless capabilities built to detect and monitor employee health and time clock.",
+		year: "2020",
+		id: "distancethermo",
+	},
+
+	// Face Masks
+	{
+		title: "&#x1F637; Face Masks Marketplace",
+		description:
+			"Give, receive, and exchange face masks with nearby community members.",
+		year: "2020",
+		id: "facemasks",
+	},
+
+	// Converge
+	{
+		title: "&#x26EA; Converge Church",
+		linkHref: "https://convergehawaii.org/",
+		linkText: "convergehawaii.org",
+		imageSrc: "assets/img/converge/converge.png",
+		description:
+			"A church website with live streaming, dynamic rendering via Pug.js, interactive calendar, user-friendly spreadsheet-database system via Airtable, photo gallery, web form validation, and responsive design.",
+		year: "2017",
+		id: "converge",
+	},
+
+	// supercart
+	{
+		title: "&#x1F955; Supercart",
+		description:
+			"An Instacart bot for shoppers (delivery drivers) that automatically finds and accepts the best delivery trips.",
+		year: "2021",
+		id: "supercart",
+	},
+
+	// Python CSV Parser
+	{
+		title: "&#x1F4CA; Python CSV Parser",
+		description: "Parse a CSV file for work hours and overtime.",
+		year: "2020",
+		id: "csvparser",
+	},
+
+	// KhanAcademy Solver
+	{
+		title: "&#x1F333; KhanAcademy Solver",
+		// "imageSrc": "https://i.pcmag.com/imagery/reviews/07AxdIVbQ63OEkJoPgCybXt-19..1594914797.png",
+		description:
+			"A Chrome extension that adds keyboard shortcuts to the Khan Academy website and auto-solves math problems.",
+		year: "2021",
+		id: "khansolver",
+	},
+
+	// Yo Karen
+	{
+		title: "&#x1F47B; Yo Karen",
+		description:
+			"A Discord bot that can solve your math homework and provide natural conversation, like Siri but with a PhD.",
+		year: "2020",
+		id: "yokaren",
+	},
+
+	// Punschedule
+	{
+		title: "&#x1F4C5; Punschedule",
+		imageSrc: "assets/img/punschedule.png",
+		description:
+			"An easy way for students to automatically generate, customize, and download their altered distance learning schedules during the COVID-19 pandemic.",
+		year: "2020",
+		id: "punschedule",
+	},
+
+	// Critter Contraption
+	{
+		title: "&#x1F408; Critter Contraption: Automatic Pet Feeder",
+		linkHref: "https://firebird1029.github.io/CC-Control/client.html",
+		linkText: "Demo Site: firebird1029.github.io/CC-Control/client.html",
+		imageSrc: "assets/img/cccontrol.png",
+		description:
+			"An early IoT project that automatically dispenses pet food at scheduled intervals or manually, all from an online web interface.",
+		year: "2016",
+		id: "cccontrol",
+	},
+
+	// Mahalograms
+	// {
+	// 	"title": "&#x1F48C; Mahalograms",
+	// 	"imageSrc": "assets/img/mahalograms.jpg",
+	// 	"description": "Mahalograms allows students to send virtual thank you cards at the end of the school year to fellow students and teachers.",
+	// 	"id": "mahalograms"
+	// },
+	// linkHref: "https://mahalograms.punschedule.com/",
+	// linkText: "mahalograms.punschedule.com",
+
+	// Smaller Projects
+
+	// OBSOLETE
+	// Robotics Lights
+	// {
+	// 	"title": "&#x1F6A5; Robotics Lights",
+	// 	// "imageSrc": "",
+	// 	"description": "Simple LED strip patterns on a robot.",
+	// 	"id": "frclights"
+	// }
+];
