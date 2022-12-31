75530857e21d61e62098c63b0214b8dc72b54210
Brandon Yee <6111102+Firebird1029@users.noreply.github.com>
2022-12-30 19:36:58 -1000

    add parallax footer to resume page

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -29,85 +29,109 @@ function parseDate(date) {
 
 export default function Resume() {
 	return (
-		<main className="app">
-			<Header />
-			<p>&nbsp;</p>
-			<div className="sticky-container">
-				<div className={styles.sectionHeader}>
-					<h2 className="blue-text" style={{ margin: 0 }}>
-						WORK EXPERIENCE
-					</h2>
+		<>
+			<main className="app">
+				<Header />
+				<p>&nbsp;</p>
+				<div className="sticky-container">
+					<div className={styles.sectionHeader}>
+						<h2 className="blue-text" style={{ margin: 0 }}>
+							WORK EXPERIENCE
+						</h2>
+					</div>
+					{resumeData.workExperience.map(
+						({
+							id,
+							jobTitle,
+							jobDescription,
+							organization,
+							dates,
+							location,
+						}) => (
+							<div key={id}>
+								<div className={styles.resumeSectionFlex}>
+									{/* Job Title + Company */}
+									<div className={styles.jobTitleCompany}>
+										<strong>{jobTitle}</strong>
+
+										<span className="hidden-touch">
+											&nbsp;&nbsp;/&nbsp;&nbsp;
+										</span>
+										<br className="hidden-desktop" />
+
+										<em>{organization}</em>
+									</div>
+
+									{/* Job Date + Location */}
+									<div>
+										<strong>{parseDate(dates.startDate)}</strong>
+										<span>&nbsp;&#8211;&nbsp;</span>
+										<strong>{parseDate(dates.endDate)}</strong>
+
+										<span className="hidden-touch">
+											&nbsp;&nbsp;/&nbsp;&nbsp;
+										</span>
+										<br className="hidden-desktop" />
+
+										<em>{location ? location.rawInput : "Remote"}</em>
+									</div>
+								</div>
+
+								{/* Job Description */}
+								<div style={{ marginTop: "-0.5rem" }}>
+									<div className="markdown" style={{ marginBottom: "2rem" }}>
+										<ReactMarkdown>
+											{jobDescription.replaceAll("\n-", "\n- ")}
+										</ReactMarkdown>
+									</div>
+								</div>
+							</div>
+						)
+					)}
 				</div>
-				{resumeData.workExperience.map(
-					({ id, jobTitle, jobDescription, organization, dates, location }) => (
+				<p>&nbsp;</p>
+				<div className="sticky-container">
+					<div className={styles.sectionHeader}>
+						<h2 className="blue-text" style={{ margin: 0 }}>
+							PROJECTS
+						</h2>
+					</div>
+					{projectData.map(({ title, description, year, id }) => (
 						<div key={id}>
 							<div className={styles.resumeSectionFlex}>
-								{/* Job Title + Company */}
 								<div className={styles.jobTitleCompany}>
-									<strong>{jobTitle}</strong>
+									<strong>{title}</strong>
 
 									<span className="hidden-touch">
 										&nbsp;&nbsp;/&nbsp;&nbsp;
 									</span>
 									<br className="hidden-desktop" />
 
-									<em>{organization}</em>
-								</div>
-
-								{/* Job Date + Location */}
-								<div>
-									<strong>{parseDate(dates.startDate)}</strong>
-									<span>&nbsp;&#8211;&nbsp;</span>
-									<strong>{parseDate(dates.endDate)}</strong>
-
-									<span className="hidden-touch">
-										&nbsp;&nbsp;/&nbsp;&nbsp;
-									</span>
-									<br className="hidden-desktop" />
-
-									<em>{location ? location.rawInput : "Remote"}</em>
+									<em>{year}</em>
 								</div>
 							</div>
-
-							{/* Job Description */}
 							<div style={{ marginTop: "-0.5rem" }}>
 								<div className="markdown" style={{ marginBottom: "2rem" }}>
-									<ReactMarkdown>
-										{jobDescription.replaceAll("\n-", "\n- ")}
-									</ReactMarkdown>
+									<ReactMarkdown>{description}</ReactMarkdown>
 								</div>
 							</div>
 						</div>
-					)
-				)}
-			</div>
-			<p>&nbsp;</p>
-			<div className="sticky-container">
-				<div className={styles.sectionHeader}>
-					<h2 className="blue-text" style={{ margin: 0 }}>
-						PROJECTS
-					</h2>
+					))}
 				</div>
-				{projectData.map(({ title, description, year, id }) => (
-					<div key={id}>
-						<div className={styles.resumeSectionFlex}>
-							<div className={styles.jobTitleCompany}>
-								<strong>{title}</strong>
-
-								<span className="hidden-touch">&nbsp;&nbsp;/&nbsp;&nbsp;</span>
-								<br className="hidden-desktop" />
-
-								<em>{year}</em>
-							</div>
-						</div>
-						<div style={{ marginTop: "-0.5rem" }}>
-							<div className="markdown" style={{ marginBottom: "2rem" }}>
-								<ReactMarkdown>{description}</ReactMarkdown>
-							</div>
-						</div>
-					</div>
-				))}
-			</div>
-		</main>
+			</main>
+			<section className={`${styles.parallax} hidden-touchs`}>
+				<div
+					style={{
+						width: "100%",
+						textAlign: "center",
+						position: "fixed",
+						bottom: 0,
+						zIndex: -1,
+					}}
+				>
+					<h3 className="unselectable">BRANDON YEE</h3>
+				</div>
+			</section>
+		</>
 	);
 }
