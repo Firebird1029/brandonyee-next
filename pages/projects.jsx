bde757b0328738fb6a9f40b18c248508f7c628f2
Brandon Yee <6111102+Firebird1029@users.noreply.github.com>
2022-12-30 21:08:23 -1000

    populate project page, add tons of styling

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,9 +1,67 @@
+import Image from "next/image";
 import Header from "../components/Header";
+import styles from "../styles/Projects.module.css";
+import { projects } from "../projects.js";
 
 export default function Projects() {
 	return (
-		<main className="app">
-			<Header />
-		</main>
+		<>
+			<main className="app">
+				<Header />
+			</main>
+			<section
+				style={{
+					// https://www.schemecolor.com/beautiful-light-pastels-color-scheme.php
+					background:
+						"linear-gradient(transparent, #DEFDE0, #DEF3FD, #FDDFDF, #FCF7DE, transparent)",
+				}}
+			>
+				<section
+					className="app app-wide"
+					style={{
+						backgroundColor: "initial",
+					}}
+				>
+					<p className="spacer hidden-touch">&nbsp;</p>
+					<div className={styles.container}>
+						{projects.map(
+							({
+								id,
+								title,
+								description,
+								year,
+								imageSrc,
+								linkHref,
+								linkText,
+							}) => (
+								<div key={id} className={`${styles.card} ${styles.glass}`}>
+									<div className={styles.hoverGradient}></div>
+									<h3>{title}</h3>
+									<p>{year}</p>
+									{imageSrc && (
+										<div className={styles.imageContainer}>
+											<Image
+												src={imageSrc}
+												fill
+												alt={title}
+												style={{ objectFit: "contain" }}
+											/>
+										</div>
+									)}
+									<p>
+										{linkHref && linkText && (
+											<a href={linkHref} target="_blank">
+												{linkText}
+											</a>
+										)}
+									</p>
+									<p>{description}</p>
+								</div>
+							)
+						)}
+					</div>
+				</section>
+			</section>
+		</>
 	);
 }
